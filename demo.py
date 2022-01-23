3fc6b07e8d0851c8e8cd040978bc84edddf09a56
Izzat <90560217+IzzatCORP@users.noreply.github.com>
2022-01-23 16:21:45 +0800

    Demo Testing
    This is a demo for testing python

---
+++ demo.py
@@ -0,0 +1,14 @@
+name=input('what is you name lol? Name:')
+
+age=input('how old are you? Age:')
+
+height=input('what is your tall height? Height:')
+
+home=input('what is your home? From:')
+
+country=input('what is your country? Country:')
+
+hobby=input('what is your favorite hobbies? Hobby:')
+
+print('verify passed , thank you for demo testing verification')
+print('testing by IzzatCORP')
